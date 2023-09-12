56b3c5fc57d4d5d66f387e5cac8945193d188743
rbrtknwls <r2knowle@uwaterloo.ca>
2023-09-12 15:27:30 -0400

    A1Q1

---
+++ A1/q1.py
@@ -0,0 +1,55 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import numpy as np
+def test():
+    x1 = (125000, 140000, 153000, 170000)
+    x2 = (122000, 130000, 150000, 155000, 158000, 170000, 172000, 127500, 181000)
+    x3 = (148000, 152000, 153000, 177000, 181000, 182000, 183000, 197000)
+    x4 = (168000, 181000, 199000, 219000)
+    x5 = (148000, 185000, 190000, 202000, 203000)
+    x6 = (152000, 152500, 180500, 195000, 198000, 221000)
+    x7 = (130000, 159000, 172000)
+    x8 = (127000, 140000, 153000, 170000)
+    x9 = (128000, 130000, 132000, 140000, 150000)
+    x10 = (128000, 130000, 132000, 140000, 150000)
+    x11 = (148000, 175000, 188000, 190000, 202000, 203000, 210000)
+    x12 = (125000, 170000)
+    x13 = (148000, 175000, 188000, 190000, 202000, 203000, 221000)
+    x14 = (151000, 160000, 171000)
+    x15 = (163000, 182000)
+    x16 = (221000)
+    x17 = (181000, 200000, 201000, 203000)
+    x18 = (152000, 160000, 170000, 171000)
+    x19 = (144000, 175000, 183000, 184000, 201000, 202000)
+    x20 = (118000, 130000, 162000)
+
+    print(np.average(x1))
+    print(np.average(x2))
+    print(np.average(x3))
+    print(np.average(x4))
+    print(np.average(x5))
+    print(np.average(x6))
+    print(np.average(x7))
+    print(np.average(x8))
+    print(np.average(x9))
+    print(np.average(x10))
+    print(np.average(x11))
+    print(np.average(x12))
+    print(np.average(x13))
+    print(np.average(x14))
+    print(np.average(x15))
+    print(np.average(x16))
+    print(np.average(x17))
+    print(np.average(x18))
+    print(np.average(x19))
+    print(np.average(x10))
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    test()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
