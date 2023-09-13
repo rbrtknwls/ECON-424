b3e476c1b677005347fc1ac353c538bde227459b
rbrtknwls <r2knowle@uwaterloo.ca>
2023-09-13 15:16:25 -0400

    A1 DONE

---
+++ A1/q2OR3.py
@@ -0,0 +1,25 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import numpy as np
+def test():
+    x = [118000, 121000, 121000, 122000, 123000, 123000, 124000, 124000,
+         130000, 130000, 131000, 132000, 133000, 133000, 137000, 139000,
+         140000, 140000, 140000, 141000, 141000, 142000, 146000, 150000,
+         151000, 151000, 151000, 151000, 160000, 160000, 160000, 160000,
+         160000, 160000, 160000, 160000, 160000, 160000, 160000, 160000,
+         165000, 165000, 165000, 170000, 170000, 170000, 170000, 170000,
+         180000, 180000, 180000, 180000, 180000, 180000, 180000, 180000,
+         182000, 182000, 182000, 182000, 185000, 185000, 185000, 185000,
+         190000, 190000, 200000, 200000, 200000, 200000, 200000, 200000,
+         200000, 200000, 210000, 222000, 220000, 220000, 220000]
+
+    print(np.average(x))
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    test()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
